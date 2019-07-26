fe50e68a6e90a5148befa939678098509190434b
vlavvs <46235944+vlavvs@users.noreply.github.com>
2019-07-26 12:12:52 +0600

    Add files via upload

---
+++ Find_Inactive_Computer.ps1
@@ -0,0 +1,21 @@
+﻿clear
+$ComputerCollection = Get-ADComputer -Filter * -Properties lastlogondate
+
+$Date = New-Object System.DateTime 
+$Date = Get-Date
+#Количество дней. (Будут показаны компьютеры последний вход которых был выполнен более указанных дней)
+$DayExpire = 30 
+$Date = $Date.AddDays($DayExpire - $DayExpire * 2) #Пеервод в отрицательное значение
+$NumberComputerFound = 0
+
+FOREACH ($Computer in $ComputerCollection)
+{
+IF ($Computer.lastlogondate -lt $date) 
+{
+Write-Host '-----'
+Write-Host $Computer.Name  ' последний вход '  $Computer.LastLogonDate.DateTime
+$NumberComputerFound++
+}
+}
+Write-Host 'Всего компьютеров: '  $NumberComputerFound
+pause
